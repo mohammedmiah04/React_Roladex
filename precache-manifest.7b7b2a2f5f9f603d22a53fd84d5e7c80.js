8aa01da78d1e970cec4d329d8bd288c8be2e63d2
Nanu Miah <nanumiah@nanus-mbp.home>
2020-08-25 15:09:59 +0100

    Updates

---
+++ precache-manifest.7b7b2a2f5f9f603d22a53fd84d5e7c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6743e09a267a361027a6d000aacfa796",
+    "url": "/React_Roladex/index.html"
+  },
+  {
+    "revision": "a9a519b330984b8dd2e3",
+    "url": "/React_Roladex/static/css/main.8406329a.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/React_Roladex/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Roladex/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a519b330984b8dd2e3",
+    "url": "/React_Roladex/static/js/main.fc819382.chunk.js"
+  },
+  {
+    "revision": "533b5a1720a11380bd9a",
+    "url": "/React_Roladex/static/js/runtime-main.8fdd8ac1.js"
+  }
+]);
